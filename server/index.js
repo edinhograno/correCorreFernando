09367f918f51ce9d5184fca27c78f8482ba18fb4
edinhograno <granomotorista@gmail.com>
2021-06-22 23:24:50 -0300

    pronto

---
+++ server/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "employeesystem",
+});
+
+app.post("/create", (req, res) => {
+  const nome = req.body.nome;
+  const idade = req.body.idade;
+  const pais = req.body.pais;
+  const genero = req.body.genero;
+  const salario = req.body.salario;
+
+  db.query(
+    "INSERT INTO employees (nome, idade, pais, genero, salario) VALUES (?,?,?,?,?)",
+    [nome, idade, pais, genero, salario],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Funcionário Cadastrado");
+      }
+    }
+  );
+});
+
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const salario = req.body.salario;
+  db.query(
+    "UPDATE SET employees salario = ? WHERE id = ?",
+    [salario, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+// app.delete()
+
+app.listen(3001, () => {
+  console.log("E ai babaca, to rodando aqui viu!");
+});
